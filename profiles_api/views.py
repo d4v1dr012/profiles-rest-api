9ca2b8f320695c64099d82242284b59f0b454a17
David <davidroizag@gmail.com>
2020-05-17 00:50:04 -0500

    added PUT POST PATCHD ELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView (APIView):
     #test API Views
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format = None):
         """Returns a list of APIViews"""
         an_apiview = [
@@ -13,3 +17,26 @@ class HelloApiView (APIView):
         ]
 
         return Response({'message': "Hello!", 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """hello messaje"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello : {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'DELETE'})
